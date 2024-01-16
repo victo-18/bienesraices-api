ca91ed285f9d57c1e32e86b04d4771ac75aecb7b
Victor Alomia <alomia18@gmail.com>
2024-01-15 23:53:42 -0500

    Generando token para la confirmacion de la cuenta

--- controlers/usuarioControler.js
+++ controlers/usuarioControler.js
@@ -1,6 +1,8 @@
 import { check, validationResult } from "express-validator";
 //Importando modelos
 import Usuario from "../models/Usuario.js";
+import { generarId } from "../helpers/token.js";
+import { emailRegistro } from "../helpers/email.js";
 //Definidiendo las rutas
 const formularioLogin = (req, res) => {
   res.render("auth/login", {
@@ -43,49 +45,66 @@ const registrar = async (req, res) => {
       },
     });
   }
- 
+
   //Validando que el usuario no exista en la db
+
   const usuarioRepetido = await Usuario.findOne({
-    where: { email: req.body.email }, 
+    where: { email: req.body.email },
   });
-  if(usuarioRepetido){
+  if (usuarioRepetido) {
     return res.render("auth/registro", {
-        pagina: "Crear cuenta",
-        errores: [{msg:"El usuario ya esta registrado"}],
-        //setiando el input
-        usuario: {
-          nombre: req.body.nombre,
-          email: req.body.email,
-        },
-      });
+      pagina: "Crear cuenta",
+      errores: [{ msg: "El usuario ya esta registrado" }],
+      //setiando el input
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email,
+      },
+    });
   }
-  
+
   //Insertando los datos a la BD
-  const usuario = await Usuario.create(req.body);
-//   console.log(req.body);
-//   res.json(usuario);
-  if(usuario){
-    return res.render("auth/registro", {
-        pagina: "Crear cuenta",
-        errores: [{msg:"Usuario creado con exito"}],
-        //setiando el input
-        usuario: {
-          nombre: req.body.nombre,
-          email: req.body.email,
-        },
-      });
+  const { nombre, email, password } = req.body;
+  const usuario = await Usuario.create({
+    nombre,
+    email,
+    password,
+    token: generarId(),
+  });
+  //Envio de email de confirmacion de cuenta
+  emailRegistro({
+    nombre: usuario.nombre,
+    email: usuario.email,
+    token: usuario.token,
+  });
+  if (usuario) {
+    res.render("mensaje/mensaje", {
+      pagina: "Cuenta creada correctamente",
+      mensaje: `Hemos enviado un Email de confrimación al E-mail ${email}, presiona en el enlace para confirmar tu cuenta`,
+    });
   }
 };
+//Confirmando cuenta
+const confirmarCuenta = (req,res)=>{
+  const {token}=req.params;//obtiene  los datos de la url
+ //Validar El token
+
+ //Confirmar la cuenta
+}
 //Recuperar passwor
 const formularioRecuperarPassword = (req, res) => {
   res.render("auth/recuperar-password", {
     pagina: "Recuperar Tu password",
   });
 };
+
+
 //exportando funciones
 export {
   formularioLogin,
   formularioRegistro,
-  formularioRecuperarPassword,
   registrar,
+  confirmarCuenta,
+  formularioRecuperarPassword,
+ 
 };
--- index.js
+++ index.js
@@ -27,7 +27,7 @@ server.use('/auth',usuarioRouter)
 server.use(express.static('public'))
 
 // Creando el puerto y arrancando el proyecto
-const port=3000;
+const port= process.env.PORT || 3000;
 server.listen(port,()=>{
     console.log(`Servidor escuchando en el puerto ${port}`)
 })
