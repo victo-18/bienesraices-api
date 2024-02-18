d3ca3911f21acb9030eeda707c1bbc648f63f941
Victor Alomia <alomia18@gmail.com>
2024-02-17 23:21:39 -0500

    utilizando JWT para la autenticacion del usuario

--- helpers/token.js
+++ helpers/token.js
@@ -1,4 +1,16 @@
+import Jwt  from "jsonwebtoken";
 const generarId =()=> (Math.random().toString(32).substring(2)+Date.now().toString(32).substring(2));
-export{
-    generarId
-}
+
+// generar un tken
+const generarJWT = ({id:id,nombre})=>(
+    Jwt.sign({
+    id:id,
+    nombre,
+   },process.env.JWT_SICRET ,{
+    expiresIn: "1d"
+   }))
+
+   export{
+    generarId,
+    generarJWT
+   }
